2cb6ec3a45f9fdadb6bd61d1e68c3c54e7f37b6b
AbiSri <AbiSri@bergen.org>
2016-01-23 15:05:45 -0500

    fix edit text 1

--- app/src/main/java/com/me/sagar/androidedittext/MainActivity.java
+++ app/src/main/java/com/me/sagar/androidedittext/MainActivity.java
@@ -52,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
                     value1.setText(df.format(0));
                     value2.setText(df.format(0));
                 } else {
+                    value1.setText(df.format(valuevar));
                     value2.setText(df.format(valuevar));
                 }
             }
